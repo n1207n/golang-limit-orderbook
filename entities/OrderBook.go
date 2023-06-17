f30ea2e1c6165a9ea2effccc2ab495638d823160
Silin Na <n1207n@gmail.com>
2023-06-17 15:28:55 -0700

    feat: Added Orderbook.Match method

--- entities/OrderBook.go
+++ entities/OrderBook.go
@@ -1,6 +1,8 @@
 package entities
 
 import (
+	"container/heap"
+	"fmt"
 	utils "github.com/n1207n/golang-limit-orderbook/utils"
 )
 
@@ -16,5 +18,42 @@ func NewOrderBook(ticker string) *OrderBook {
 		ticker: ticker,
 		bids:   make(utils.PriorityQueue, 0),
 		asks:   make(utils.PriorityQueue, 0),
+	}
+}
+
+func intMin(a int, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func (ob *OrderBook) Match() {
+	for ob.bids.Len() > 0 && ob.asks.Len() > 0 {
+		buy := heap.Pop(&ob.bids).(*LimitOrder)
+		sell := heap.Pop(&ob.asks).(*LimitOrder)
+
+		// Can't fulfill the matching push these orders back to the orderbook
+		if buy.Price < sell.Price {
+			heap.Push(&ob.bids, buy)
+			heap.Push(&ob.asks, sell)
+			break
+		}
+
+		quantity_filled := intMin(buy.Quantity, sell.Quantity)
+		fmt.Printf("Matched %d shares at %d\n", quantity_filled, sell.Price)
+
+		buy.Quantity -= quantity_filled
+		sell.Quantity -= quantity_filled
+
+		// Order lots are partially fulfilled
+		if buy.Quantity > 0 {
+			heap.Push(&ob.bids, buy)
+		}
+
+		if sell.Quantity > 0 {
+			heap.Push(&ob.asks, sell)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,9 @@ module github.com/n1207n/golang-limit-orderbook
 go 1.20
 
 require github.com/google/uuid v1.3.0
+
+require (
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+)
