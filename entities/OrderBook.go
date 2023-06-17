83e9a2e736d1d78bcc2902beed1752869271ee9a
Silin Na <n1207n@gmail.com>
2023-06-17 15:08:56 -0700

    feat: Added a constructor for OrderBook struct

--- entities/OrderBook.go
+++ entities/OrderBook.go
@@ -5,6 +5,16 @@ import (
 )
 
 type OrderBook struct {
-	bids utils.PriorityQueue
-	asks utils.PriorityQueue
+	ticker string
+	bids   utils.PriorityQueue
+	asks   utils.PriorityQueue
+}
+
+// NewOrderBook returns a new instance of OrderBook for a ticker
+func NewOrderBook(ticker string) *OrderBook {
+	return &OrderBook{
+		ticker: ticker,
+		bids:   make(utils.PriorityQueue, 0),
+		asks:   make(utils.PriorityQueue, 0),
+	}
 }
